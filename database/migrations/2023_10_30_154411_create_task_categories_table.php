3ed2277b629ff7bbb49923e39fac5cd7f270e2b8
Josh Karta <josh.kartadiwirja@kabinetpresident.gov.sr>
2023-10-30 12:44:50 -0300

    initial commit

---
+++ database/migrations/2023_10_30_154411_create_task_categories_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('task_categories', function (Blueprint $table) {
+            $table->integer('task_id');
+            $table->integer('category_id')->index('category_id');
+
+            $table->primary(['task_id', 'category_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('task_categories');
+    }
+};
